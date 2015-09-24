7bda066a77c87cb4ece4efc3b8af5d6c388bb19d
Amit Bhagat <abhagat@amazon.com>
2015-09-23 23:41:41 -0700

    scatter plot example

---
+++ matplotlib_ex4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from matplotlib import pyplot as plt
+
+size = [1700,2100,1900,1300,1600,2200]
+dollars = [53000,44000,59000,82000,50000,68000]
+
+plt.scatter(size,dollars)
+plt.show()
